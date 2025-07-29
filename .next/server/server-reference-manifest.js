c2fcf377d308651f3062b166ae965da974e2e5ab
Guilherme <guirs2602@gmail.com>
2025-07-28 21:08:59 -0300

    install dependences

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"iwE6KnGeS/F2RjqSpKtg/NhPASsAK4X4zhF3lrOCNn8=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
